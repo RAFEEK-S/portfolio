d90409d1a028ef31173e4861c2d8708b1abb5e47
RAFEEK-S <rafeeksarthar73@gmail.com>
2023-03-15 12:45:17 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
+import './img.jpg';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <section id='aboutus'>
+        <div className= "row align-items-centre mt-5">
+          
+        <div className= "col-md-6 ">
+        <h1 className='title-centre pt-5 px-3'>About us </h1>
+          <h2 className=' title-centre px-3' >I am Rafeek</h2>
+          <p className='text-centre px-3' >It has survived not only five centuries, but also the leap into electronic typesetting, 
+            remaining essentially unchanged. It was popularised in the 1960s with 
+            the release of Letraset sheets containing Lorem Ipsum passages, and more recently with 
+            desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum</p>
+            <button id ="buttons" type="button" class="btn btn-outline-success mx-3">Resume</button>
+          </div>
+          <div className= "col-md-6  py-5 px-5">
+          <img src="https://www.thedesigngrouponline.com/wp-content/uploads/2012/04/shutterstock_1122339353.jpg" class="img-fluid" alt="..."/>
+        </div>
+          
+        </div>
+        </section>
+      </div>
     </div>
   );
 }
